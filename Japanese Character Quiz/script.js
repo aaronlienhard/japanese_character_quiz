ba79e5c580bae48dfad7758076b06a82e37dffd5
aaronlienhard <90061558+aaronlienhard@users.noreply.github.com>
2021-10-23 14:58:24 -0400

    Added About Page (Light + Dark)

--- Japanese Character Quiz/script.js
+++ Japanese Character Quiz/script.js
@@ -1,20 +1,16 @@
 var questionAmount = 10;
 
-const Ten_Questions_Button = document.querySelector('#Ten_Questions_Button');
-const Fifteen_Questions_Button = document.querySelector('#Fifteen_Questions_Button');
-const Twenty_Questions_Button = document.querySelector('#Twenty_Questions_Button');
-
-Ten_Questions_Button.onclick = function() {
+function updateAmount10() {
     questionAmount = 10;
     alert("Question amount is " + questionAmount);
 }
 
-Fifteen_Questions_Button.onclick = function() {
+function updateAmount15() {
     questionAmount = 15;
     alert("Question amount is " + questionAmount);
 }
 
-Twenty_Questions_Button.onclick = function() {
+function updateAmount20() {
     questionAmount = 20;
     alert("Question amount is " + questionAmount);
 }
